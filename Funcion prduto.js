e2965ee2e99a9bf81081b7c9372595b508b33624
Mark <114485292+Markxd02@users.noreply.github.com>
2023-03-20 11:13:13 +0100

    Update and rename Funcion prduto.js to Function prduto.js

--- Funcion prduto.js
+++
@@ -1,6 +0,0 @@
-let nombre = "Mario";
-function saluda(nombre){
-    return "hola" + nombre + "¿Que tal estas?"
-}
-let msn = saluda(Mario);
-console.log(msn)
---
+++ Function prduto.js
@@ -0,0 +1,5 @@
+function producto(x1,x2){
+    return x1 * x2;
+}
+let resultado = producto(3,7);
+console.log("El resultado de multiplicar 3 y 7 es"+ res)
